11b1b3523af2cf5ce4ef9bdbf90b4d69dee3af75
erlendandre <erlendandre@icloud.com>
2026-04-15 13:32:32 +0200

    change to V30

--- backend/src/main/resources/db/migration/V30__add_collective_achievement_config.sql
+++ backend/src/main/resources/db/migration/V30__add_collective_achievement_config.sql
@@ -1,4 +1,4 @@
-CREATE TABLE collective_enabled_achievements (
+CREATE TABLE IF NOT EXISTS collective_enabled_achievements (
     collective_id BIGINT NOT NULL REFERENCES collectives(id) ON DELETE CASCADE,
     achievement_key VARCHAR(50) NOT NULL,
     PRIMARY KEY (collective_id, achievement_key)
